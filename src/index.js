0619b42a0253d6205c64df59ef1f15750b3b0703
centinela1024tb <centinela1024tb@gmail.com>
2023-10-06 17:47:03 -0600

    [ADD] config server express and configure ESM (use import/from)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hola node')
+import express from 'express';
+const PORT = 4000
+
+const app = express()
+
+console.log('desde index.js ')
+
+
+app.listen(PORT, () => {
+   console.log(`Server run on Port ${PORT}`)
+})
