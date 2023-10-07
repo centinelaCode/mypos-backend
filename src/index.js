d442f6f9321e691ef166ee2ddcb672a991de7c03
centinela1024tb <centinela1024tb@gmail.com>
2023-10-07 13:07:44 -0600

    [ADD] Enable dotenv and express.json

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv'
+
 import usuarioRoutes from './routes/usuariosRoutes.js'
 
 const PORT = process.env.PORT || 4000;
 
 const app = express()
+dotenv.config();                           //! Enable dotenv
+app.use(express.json());                   //! Enable parcer
+
+
 
 //! ======= Routing =======
 app.use('/api/usuarios', usuarioRoutes)
