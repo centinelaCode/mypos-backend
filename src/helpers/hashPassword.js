4238347f430204b669a1f5f2dd1fba05226abac5
centinela1024tb <centinela1024tb@gmail.com>
2023-10-09 11:26:58 -0600

    [ADD] change the way to export function for hash password

--- src/helpers/hashPassword.js
+++ src/helpers/hashPassword.js
@@ -1,9 +1,12 @@
 import bcrypt from 'bcrypt'
 
-export async function hashPassword(password) {
+async function hashPassword(password) {
    // let passwordHash
    const salt = await bcrypt.genSalt(10)
    return password = await bcrypt.hash(password, salt)
 }
 
 
+export {
+   hashPassword,
+}
