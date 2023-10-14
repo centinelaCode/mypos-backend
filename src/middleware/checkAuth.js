5d24f27eed77f5a905e5d580dd6c7063329507d7
centinela1024tb <centinela1024tb@gmail.com>
2023-10-14 14:44:37 -0600

    [ADD] create middleware checkAuth for validate JWT and protect routes

---
+++ src/middleware/checkAuth.js
@@ -0,0 +1,51 @@
+import { PrismaClient } from '@prisma/client'
+import jwt from 'jsonwebtoken'
+
+const db = new PrismaClient()
+
+const checkAuth = async(req, res, next) => {
+   let token;
+
+   if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+      try {
+         token = req.headers.authorization.split(' ')[1]
+
+         const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+         // if(!decoded.id) {
+         //    const error = new Error('Error, Token Invalido')
+         //    return res.status(404).json({ msg: error.message })
+         // }
+
+         //? creamos en el req la variable usuario y con payload(id) obtenemos el usuario
+         req.usuario = await db.usuario.findFirst({
+            where: {
+               id: decoded.id
+            },
+            select: {
+               id: true,
+               nombre: true,
+               email: true,
+            }
+         })
+         await db.$disconnect()
+
+         console.log(req.usuario)
+
+         return next()
+      } catch (error) {
+         await db.$disconnect()
+         return res.status(404).json({ msg: 'Hubo un error'})
+      }
+   }
+
+   //? cuando no se envia el token
+   if(!token) {
+      const error = new Error('Token no válido')
+      res.status(401).json({ msg: error.message })
+   }
+
+   next()
+}
+
+export default checkAuth
